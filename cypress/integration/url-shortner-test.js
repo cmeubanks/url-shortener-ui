42cff3f11192a1eaf4525f4647e7b2d42bb94177
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-15 10:23:20 -0600

    define beforeEach in tests

--- cypress/integration/url-shortner-test.js
+++ cypress/integration/url-shortner-test.js
@@ -1,5 +1,14 @@
 describe('Main Page Tests', () => {
 
 
-  beforeEach(() =>)
+  beforeEach(() => {
+    cy.fixture('url-data.json')
+      .then(urls => {
+        cy.intercept('http://localhost:3001/api/v1/urls', {
+          statusCode: 200,
+          body: urls
+        })
+      })
+      cy.visit('http://localhost:3000/')
+  })
 })
