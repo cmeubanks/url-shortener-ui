877a239a57fceaf8dace7b8a6d4dcb9978c85a56
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-15 10:37:11 -0600

    test page load

--- cypress/integration/url-shortner-test.js
+++ cypress/integration/url-shortner-test.js
@@ -17,6 +17,13 @@ describe('Main Page Tests', () => {
     it('should display page elements and existing server data on main page load', () => {
       cy.get('h1')
         .contains('URL Shortener')
+        .get('.url').should('have.length', 1)
+        .get('.url').eq(0).get('h3')
+          .contains('Awesome stub')
+        .get('.url').eq(0).get('a')
+          .contains('http://localhost:3001/useshorturl/1')
+        .get('.url').eq(0).get('p')
+          .contains('https://this-is-a-stub.com/1324928349872')
     })
   })
 })
